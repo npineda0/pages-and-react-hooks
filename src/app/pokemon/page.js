e36215d1b869d313ea9091a0eb69335667f7cd15
npineda0 <97705502+npineda0@users.noreply.github.com>
2024-09-18 09:43:07 -0400

    created pokemon page

---
+++ src/app/pokemon/page.js
@@ -0,0 +1,8 @@
+export default function Pokemon() {
+    return(
+        <main>
+            {/* can only have one top level element */}
+            <h1>Pokemon Page</h1>
+        </main>
+    );
+}
